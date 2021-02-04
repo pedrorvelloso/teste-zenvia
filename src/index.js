59aa114e4979b35a4d62da01fbd44528258d6af4
Pedro Reis <pedro_reis14@hotmail.com>
2021-02-04 12:30:02 -0300

    arrumo typo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const expres = require('express')
+const express = require('express')
 const routes = require('./routes')
 
-const app = expres()
+const app = express()
 
-app.use(expres.json())
+app.use(express.json())
 app.use(routes)
 
 app.listen(5000, () => {
